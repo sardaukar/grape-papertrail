85752a59aa5dfc9a0fa6defa744da743fe290cfa
Miles Zimmerman <miles@maztek.io>
2015-03-12 16:04:14 -0700

    Add set_paper_trail_controller_info

--- lib/grape/papertrail.rb
+++ lib/grape/papertrail.rb
@@ -4,9 +4,13 @@ module Grape
     def self.included(base)
       Grape::Endpoint.class_eval do
 
-        def set_papertrail_user user
-	  ::PaperTrail.whodunnit = user
+        def set_papertrail_user(user)
+          ::PaperTrail.whodunnit = user
         end
+        
+        def set_paper_trail_controller_info(info)
+          ::PaperTrail.controller_info = info
+      	end
 
       end
     end
